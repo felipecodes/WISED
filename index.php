62102a41bf53068e2d1ba2472e041a6e6291e979
DoughxD <douglassampaio23@gmail.com>
2018-12-14 11:21:51 -0300

    Initial commit
    primeiro commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+<link href = "principal.css" rel = "stylesheet" type = "text/css">
+</head>
+
+<body>
+<h1>olá mundo</h1>
+
+<div id = div1>
+
+<?php 
+$teste = 'uhuu5';
+echo ($teste);
+?>
+
+</div>
+
+</body>
+
+
+
+
+
+</html>
